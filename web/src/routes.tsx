7af8841c2169af81ff61976ca41e75e12637f64d
Alex <64551315+Alex-Ferreira-Santos@users.noreply.github.com>
2020-11-23 14:59:36 -0300

    folder structure

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -3,9 +3,12 @@ import { BrowserRouter, Switch, Route} from "react-router-dom";
 
 const Routes: React.FC = () => {
   return(
-    <div>
-        <h1>teste aaaa</h1>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route></Route>
+      </Switch>
+    </BrowserRouter>
+    
   )
 }
 
