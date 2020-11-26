7ae5b31c8dd96d19fc69e7b54e49a331524ff056
Alex <64551315+Alex-Ferreira-Santos@users.noreply.github.com>
2020-11-26 10:46:35 -0300

    html da nav

--- web/src/components/Nav/index.tsx
+++ web/src/components/Nav/index.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { Container, Inputs } from './styles';
 
-import { Container } from './styles';
+import lupa from "../../img/magnifying-glass.svg"
+import carrinho from "../../img/shopping-cart.svg"
+import burguer from "../../img/cardapio.svg"
+
+
 
 const Nav: React.FC = () => {
   return (
     <Container>
-      <h1>Nav</h1>
+      <nav>
+        <h1>LOGO</h1>
+        <Inputs>
+          <input type="search" name="pesquisa" id="pesquisa" placeholder="pesquisa..."/>
+          <button type="submit"><img src={lupa} alt="lupa"/></button>
+        </Inputs>
+        <Link to=""><img src={carrinho} alt="carrinho"/></Link>
+        <button><img src={burguer} alt=""/></button>
+        <p>olá usuario</p>
+      </nav>
     </Container>
   );
 };
--- web/src/components/Nav/styles.ts
+++ web/src/components/Nav/styles.ts
@@ -3,3 +3,13 @@ import styled from 'styled-components';
 export const Container = styled.div`
 
 `;
+
+export const Inputs = styled.div`
+    height: 3em;
+    background-color: lightblue;
+
+    img{
+        width: 1em;
+        height: 1em;
+    }
+`;
