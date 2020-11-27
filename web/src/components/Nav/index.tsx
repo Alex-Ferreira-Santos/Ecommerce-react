646cd19d837c36e98608d83d0ec3fb617e98f4a5
Alex <64551315+Alex-Ferreira-Santos@users.noreply.github.com>
2020-11-27 11:45:31 -0300

    estilo da nav

--- web/src/components/Nav/index.tsx
+++ web/src/components/Nav/index.tsx
@@ -4,24 +4,43 @@ import { Container, Inputs } from './styles';
 
 import lupa from "../../img/magnifying-glass.svg"
 import carrinho from "../../img/shopping-cart.svg"
-import burguer from "../../img/cardapio.svg"
+
 
 
 
 const Nav: React.FC = () => {
+
+  let open = true;
+  const btn = document.querySelector(".menu")
+  btn?.addEventListener("click",() => {
+    if (open) {
+      btn.setAttribute("Class","menu open");
+      open = false
+    }else{
+      btn.removeAttribute("class");
+      btn.setAttribute("class","menu");
+      open = true
+    }
+    
+  })
+
   return (
-    <Container>
-      <nav>
-        <h1>LOGO</h1>
-        <Inputs>
-            <input type="search" name="pesquisa" id="pesquisa" placeholder="pesquisa..."/>
-            <button type="submit" className="search"><img src={lupa} className="lupa" alt="lupa"/></button>
-        </Inputs>
-        <Link to=""><img src={carrinho} className="carrinho" alt="carrinho"/></Link>
-        <button className="menu"><img src={burguer} className="burguer" alt="menu"/></button>
-        
-      </nav>
-    </Container>
+      <Container>
+        <nav>
+          <h1>LOGO</h1>
+          <Inputs>
+              <input type="search" name="pesquisa" id="pesquisa" placeholder="pesquisa..."/>
+              <button type="submit" className="search"><img src={lupa} className="lupa" alt="lupa"/></button>
+          </Inputs>
+          <Link to="" className="link"><img src={carrinho} className="carrinho" alt="carrinho"/></Link>
+          <button className="menu">
+            <div className="one"></div>
+            <div className="two"></div>
+            <div className="three"></div>
+          </button>
+          <p className="ola">Olá usuario</p>
+        </nav>
+      </Container>
   );
 };
 
--- web/src/components/Nav/styles.ts
+++ web/src/components/Nav/styles.ts
@@ -24,25 +24,70 @@ export const Container = styled.div`
         width: 2em;   
     }
 
+    .link{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .menu{
+        
+        cursor: pointer;
+        border: none;
+        background-color: transparent;
+        margin-left: 0.8em;
+        width: 40px;
+        
+    }
+
+    .one,.two,.three{
+        background-color: black;
+        height: 5px;
+        width: 100%;
+        margin: 6px auto;
+        transition-duration: 0.3s;
+    }
+
+    .menu.open .one{
+        transform: rotate(45deg) translate(7px,7px);
+    }
+
+    .menu.open .two{
+        opacity: 0;
+    }
+
+    .menu.open .three{
+        transform: rotate(-45deg) translate(8px,-9px);
+    }
+
+    .ola{
+        position: absolute;
+        margin-left: 40%;
+        top: 2.2em;
+    }
     
 `;
 
 export const Inputs = styled.div`
-    
+
     display: flex;
     align-items: center;
-    justify-content: center;
+    width:100%;
     
+    
+
 
     #pesquisa{
         border: none;
         padding-inline-start: 3px;
         border-radius: 20px 0 0 20px;
         outline: none;
+        width:100%;
 
         &:focus{
-            box-shadow: 1px 1px 1px black;
+            border: 0.5px solid orange;
         }
+        
     }
 
     .search{
@@ -53,12 +98,17 @@ export const Inputs = styled.div`
         justify-content: center;
         height: 1.5em;
         border-radius: 0 20px 20px 0;
+        outline: none;
 
+        &:focus{
+            border: 0.5px solid orange;
+        }
     }
 
     .lupa{
         position:relative;
-        right: 1em;
+        right: 0.5em;
+        cursor: pointer;
     }
 
     img{
